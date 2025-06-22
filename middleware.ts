b08f197e5a0e4c4126402f41279a1f2d9bc881e2
vzhytnyk <vzhytnyk@redrocket.software>
2025-06-22 22:13:10 +0300

    trigger redeploy

--- middleware.ts
+++ middleware.ts
@@ -1,27 +1,26 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { handleSessionOnEdge } from '@frontegg/nextjs/edge';
+import { NextRequest, NextResponse } from "next/server";
+import { handleSessionOnEdge } from "@frontegg/nextjs/edge";
 
 export const middleware = async (request: any) => {
   const { pathname, searchParams } = request.nextUrl;
   const headers = request.headers;
-  const ip = request?.ip
-  console.log(`ip: `, ip)
+  const ip = request?.ip;
+  console.log(`ip: `, ip);
   // Additional logic if needed
-  
-  if (process.env.FRONTEGG_FORWARD_IP === 'true') {
-    console.log('headers' , headers);
+
+  if (process.env.FRONTEGG_FORWARD_IP === "true") {
+    console.log("inside FRONTEGG_FORWARD_IP middleware");
     // headers.set('x-forwarded-for', ip || headers.get('x-real-ip') || '');
-    headers.set('x-forwarded-for', 'some ip');
+    headers.set("x-forwarded-for", "testttt");
   }
-  
-  console.log("X-Forwarded-For", headers.get('x-forwarded-for'))
-  console.log("x-real-ip", headers.get('x-real-ip'))
+
+  console.log("X-Forwarded-For", headers.get("x-forwarded-for"));
+  console.log("x-real-ip", headers.get("x-real-ip"));
 
   return handleSessionOnEdge({ request, pathname, searchParams, headers });
-
 };
 
 export const config = {
-  matcher: '/(.*)',
+  matcher: "/(.*)",
 };
 // 9.2.2-alpha.13540668007
