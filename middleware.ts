c7acaab0f33f7a0422ee1087d645cf747032def9
vzhytnyk <vzhytnyk@redrocket.software>
2025-06-24 17:27:18 +0300

    test1

--- middleware.ts
+++ middleware.ts
@@ -4,18 +4,13 @@ import { handleSessionOnEdge } from "@frontegg/nextjs/edge";
 export const middleware = async (request: any) => {
   const { pathname, searchParams } = request.nextUrl;
   const headers = request.headers;
-  const ip = request?.ip;
-  console.log(`ip: `, ip);
   // Additional logic if needed
 
-  if (process.env.FRONTEGG_FORWARD_IP === "true") {
-    console.log("inside FRONTEGG_FORWARD_IP middleware");
-    // headers.set('x-forwarded-for', ip || headers.get('x-real-ip') || '');
-    headers.set("x-forwarded-for", "testttt");
-  }
-  console.log("process.env.VERCEL", process.env.VERCEL);
-  console.log("X-Forwarded-For", headers.get("x-forwarded-for"));
-  console.log("x-real-ip", headers.get("x-real-ip"));
+  // if (process.env.FRONTEGG_FORWARD_IP === "true") {
+  //   console.log("inside FRONTEGG_FORWARD_IP middleware");
+  //   // headers.set('x-forwarded-for', ip || headers.get('x-real-ip') || '');
+  //   headers.set("x-forwarded-for", "testttt");
+  // }
 
   return handleSessionOnEdge({ request, pathname, searchParams, headers });
 };
@@ -23,4 +18,6 @@ export const middleware = async (request: any) => {
 export const config = {
   matcher: "/(.*)",
 };
-// 9.2.2-alpha.13540668007
+// Old with header logs 9.2.2-alpha.13994474816
+
+// New 9.2.8-alpha.15853164109
