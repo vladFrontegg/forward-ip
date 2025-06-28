6a0270c2266cb103d30260c1811d66e96529b6e5
vzhytnyk <vzhytnyk@redrocket.software>
2025-06-28 19:16:32 +0300

    add new header log

--- middleware.ts
+++ middleware.ts
@@ -9,6 +9,8 @@ export const middleware = async (request: any) => {
   headers.set("x-forwarded-for", "test123");
   headers.set("x-frontegg-test-header", "test123");
   console.log("headers my-test-header", headers.get("x-frontegg-test-header"));
+  const cfViewer = headers.get("cloudfront-viewer-address");
+  console.log("cloudfront-viewer-address", cfViewer);
 
   return handleSessionOnEdge({ request, pathname, searchParams, headers });
 };
