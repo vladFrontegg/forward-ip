afd2bf80b36ac3251ce7e6a6b45c1528ff640830
vzhytnyk <vzhytnyk@redrocket.software>
2025-08-05 10:52:52 +0300

    test10

--- pages/api/frontegg/[...frontegg-middleware].ts
+++ pages/api/frontegg/[...frontegg-middleware].ts
@@ -1,7 +1,11 @@
 import { FronteggApiMiddleware } from "@frontegg/nextjs/middleware";
 import { NextApiRequest } from "next";
 
-export default FronteggApiMiddleware;
+export default FronteggApiMiddleware.withOptions({
+  getClientIp: (req: NextApiRequest) => {
+    return "111.111.111107";
+  },
+});
 
 export const config = {
   api: {
